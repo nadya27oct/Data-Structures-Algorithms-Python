99a385188846211ce572fc2e1d5885e75110adb9
Nadya Wanigasundara <nadyawanigasundara@Nadyas-MacBook-Pro.local>
2022-08-17 17:39:36 -0700

    adding big o(n squared).

---
+++ BigO/O(n^2).py
@@ -0,0 +1,19 @@
+boxes = [1,2,3,4,5]
+
+
+def create_all_pairs(array):
+
+    pairs = []
+    for i in range(len(array)):
+        for j in range(len(array)):
+            pairs.append([array[i],array[j]])
+    print(pairs)
+create_all_pairs(boxes)
+
+'''
+Run Time Operations - On^2
+Nested Loops are O(n * n) - Quadratic time
+Every time number of elements increase, operations increase quadratically.
+When each element of a collection/array is compared with each element from another collection,
+run time operation is O(a*b).
+'''
