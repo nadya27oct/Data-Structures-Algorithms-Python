624d5285855baac1b3e9d9a401a1a5ae7aed2dcd
Nadya Wanigasundara <nadyawanigasundara@Nadyas-MacBook-Pro.local>
2022-09-02 14:10:03 -0700

    adding comments.

--- Algorithms/Sorting/Insertion-Sort.py
+++ Algorithms/Sorting/Insertion-Sort.py
@@ -1,5 +1,6 @@
 """
-Implement insertion sort. Compare first 2 elements
+Implement insertion sort. Compare each element with next and if it is higher, compare with previous element before that
+and move it to the front.
 Assume x=[7,12,90,3].
 Compare x[0] < x[1] : nothing happens.
         x[1] < x[2] : nothing happens
@@ -20,6 +21,7 @@ i=[0,1,2,3]
 def insertSort(array):
 
     for i in range(len(array)-1):
+#First we loop over the array. If element is greater than next element, switch positions and keep checking if it is higher than previous element.
         if array[i] > array[i+1]:
             array[i+1],array[i] = array[i],array[i+1]
 
@@ -29,5 +31,7 @@ def insertSort(array):
 
     return array
 
+
+# Time complexity - O(n^2) but can be O(n) if array is nearly sorted.
 numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 insertSort(numbers)
