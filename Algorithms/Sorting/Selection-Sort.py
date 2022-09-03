1295723a8d3a6bdef278b3ebdc1fb31b297b3e9f
nadya27oct <nwanigas@gmail.com>
2022-09-03 12:25:19 -0700

    adding comments.

--- Algorithms/Sorting/Selection-Sort.py
+++ Algorithms/Sorting/Selection-Sort.py
@@ -1,11 +1,13 @@
 """
 Implement a selection sorting algorithm.
-Scanning list of items for smallest element and swap the smallest to beginning of the list.
-Loop through list twice to compare each element with one another.
-x = [4,5,3,0]
-i=0=>8 is min. Then loop through elements from i+1 to last element comparing each to min.
-Update min after each loop.
-min = i = 0; check if x[0]>x[1]. Then min = i = 1
+Scan an array for smallest element and swap the smallest to beginning of the list - which becomes sorted partition.
+Currnt minimum is the first number in unsorted partition. Then we progress through array looking for the minimum number.
+a = [6, 44, 99, 2, 3]
+Initially, 6 is minumum. We loop through array and find that 2 is the smallest number.
+We swap 6 and 2: a = [2, 44, 99, 6, 3]. We now have 2 in sorted partition.
+Moving to next iteration, 44 is current minmum and scan through remaining unsorted partition to find the smallest number - 3.
+Swap 44 and 3: a = [2, 3, 99, 6, 44].
+Then 99 is the current min and as we loop through entire array, 6 become min: a = [2, 3, 6, 99, 44].
 """
 
 def selection_sort(array):
@@ -13,7 +15,7 @@ def selection_sort(array):
     items = len(array)
     for i in range(items-1):
         min = i #0
-        temp = array[i] # 8
+        temp = array[i] # 8         array=[4,5,3,0]
         for j in range(i+1,items): #range(1,4):j=1,j=2, j=3
             if array[min] > array[j]: # a[0]=8 > a[1]=5, a[1]=5 > a[2]=4, a[2]=4>a[3]=1
                 min = j              #min=5-->i=1, min=4..i=2...min=1..i=3=min
@@ -28,4 +30,4 @@ def selection_sort(array):
 list = [6, 44, 99, 2, 3]
 selection_sort(list)
 
-# Time complexity - O(n^2) as we have tested for loops.
+# Time complexity - O(n^2) as we have nested loops.
