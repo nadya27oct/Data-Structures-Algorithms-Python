a2d792493dcecf01c6e472dd3f719aadcb2352f8
nadya27oct <nwanigas@gmail.com>
2022-09-05 17:11:20 -0700

    validate bst through in-order traversal

--- Algorithms/Traversal/Validate-Binary-Search-Tree.py
+++ Algorithms/Traversal/Validate-Binary-Search-Tree.py
@@ -2,12 +2,14 @@
 Given the root of a tree, determine whether it is a valid binary search tree.
 The left subtree of a node contains only nodes with keys less than the node's key.
 The right subtree of a node contains only nodes with keys greater than the node's key.
+We need to walk through all nodes of the tree to ensure that it is valid BST.
 Both the left and right subtrees must also be binary search trees.
   5
 1   4
    3  6
 Is left subtree a bst? 1<5. Yes and No child nodes after 1.Yes
 Is right subtree a bst? 4<5. No. Not a BST.
+Run time O(n) as we have to visit each node. Space complexity - O(n) massive stack each time we do recursive call.
 """
 
 class Solution:
@@ -33,16 +35,19 @@ If an element is not greater than previous, then it is NOT a bst.
 
 def isvalidBST(self,root):
 
-    def inorder(node,list=[]):
+    list = []
+    inorder(root)
+
+    for i in range(1,len(list)):
+        if list[i] <= list[i-1]:
+            return False
+    return True
+
+    def inorder(node):
+        nonlocal list
         if node == None:
             return True
 
-        if node.left != None:
-            inorder(node.left,list)
+        inorder(node.left)
         list.append(node.val)
-
-        if list[-1] < list[-2]:
-            return False
-
-        if node.right != None:
-            inorder(node.right)
+        inorder(node.right)
