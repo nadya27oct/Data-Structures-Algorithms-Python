fa08750237dd90b1f9fdbee57d8c42ffdd1a0e5a
nadya27oct <nwanigas@gmail.com>
2022-09-30 13:53:49 -0700

    implement recursive reverse singly lists.

--- DataStructures/LinkedLists/LL-Traversals.py
+++ DataStructures/LinkedLists/LL-Traversals.py
@@ -54,11 +54,11 @@ def linked_list_array(head):
 
     return arr
 
-print('iterative_array',linked_list_array(a))
+linked_list_array(a)
 
 q = Node("q")
-print('iterative_array',linked_list_array(q)) # -> [ 'q' ]
-print('iterative_array',linked_list_array(None))
+linked_list_array(q) # -> [ 'q' ]
+linked_list_array(None)
 
 def recursive_linked_list_array(head):
     arr = []
@@ -72,7 +72,7 @@ def fill_values(head,arr):
     arr.append(head.val)
     fill_values(head.next,arr)
 
-print('recursive_array',recursive_linked_list_array(a))
+recursive_linked_list_array(a)
 
 # Recursion is spilt into two functions, so we do not have to create multiple arrays whenever recursion function is called.
 
@@ -173,7 +173,136 @@ recursive_linked_list_find(node1, "jason")
 
 
 """
-Write a function, get_node_value, that takes in the head of a linked list and an index.
-The function should return the value of the linked list at the specified index.
+Write a function to return the value of the linked list at the specified index.
 If there is no node at the given index, then return None.
+
+# a -> b -> c -> d
+get_node_value(a, 2) = 'c'
+get_node_value(a, 7) = None
+
+For recursive solution, we have 2 base cases. When node is Null, return null.
+Affirmative base case: when count == index, return node value.
+
+traverse_ll(c,2,2) --> return 'class'
+traverse_ll(b,2,1)
+traverse_ll(a,2,0)
 """
+
+def get_node_value(head,index): # Time complexity: O(n); Space: O(1)
+
+    count = 0
+    current = head
+
+    while current!=None:
+        if count == index:
+            return current.val
+        count +=1
+        current = current.next
+    return
+
+get_node_value(a,2)
+get_node_value(a,7)
+
+def recursive_node_value(head,index):
+
+    count = 0
+    return traverse_ll(head,index,count)
+
+def traverse_ll(head,index,count):
+    if head == None:
+        return
+
+    if count == index:
+        return head.val
+
+    count += 1
+    return traverse_ll(head.next,index,count)
+
+print('recursive',recursive_node_value(a,2))
+print('recursive',recursive_node_value(a,7))
+
+# Another recursive method: start at index = 2 and return node value corresponding to index = 0.
+def recursive_node_value_1(head, index):
+
+    if head == None:
+        return
+
+    if index == 0:
+        return head.val
+
+    return recursive_node_value_1(head.next,index-1)
+
+print('2nd_recursive',recursive_node_value_1(a,3))
+print('2nd_recursive',recursive_node_value_1(a,0))
+print('2nd_recursive',recursive_node_value_1(a,7))
+
+"""
+Reverse a linked list. Return new head of linked list.
+      A->B->C->D->null
+null<-A<-B<-C<-D
+
+We need a previous pointer. At the end, A is tail. So A.next -> Null and D is head node.
+Stop when current is at null.
+
+At the start, current -> head -> A. previous -> null. next -> B
+At B, current -> head = B
+     current.next = A       A<-B
+     previous -> A
+
+At C,  current -> C
+       current.next = previous = B  A<-B<-C
+
+At D, current -> D
+      previous = C A<-B<-C<-D
+
+current.next = Null. Loop ends. Previous = D.
+
+At each step, we need to point current node to its previous.
+"""
+a = Node("A")
+b = Node("B")
+c = Node("C")
+d = Node("D")
+e = Node("E")
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+def reverse_list(head): #A->B->C->D->null
+
+    current = head      #current = A
+    previous = None     #previous = Null, current.next =B
+
+    while current != None:
+        next = current.next #B. Do not want to lose pointer from A.
+        current.next = previous # Reverse. Point current node to previous node. Now previous becomes the current node we are sitting
+        previous = current  #Advance previous and current nodes.
+        current = next #current = B
+
+    return previous
+
+#reverse_list(a)
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+def recursive_reverse(head,prev=None):
+
+    if head == None:
+        print(prev.val)
+        return prev
+
+    next = head.next # next=b
+    head.next = prev # current = a; a->Null
+    prev = head
+    return recursive_reverse(next,prev)
+
+recursive_reverse(a)
