5566b442087bd556c59455a17934e1581f802c10
nadya27oct <nwanigas@gmail.com>
2022-10-09 12:17:58 -0700

    Modifications to comments.

--- DataStructures/Graphs/DFS-BFS.py
+++ DataStructures/Graphs/DFS-BFS.py
@@ -8,8 +8,12 @@ c->e
  Iterate through each neighbor.
  BFS: Initialize queue with source node. While queue is not empty, remove from front of queue.
  queue = ['a'], pop queue. visited = ['a']
- Add neighbors of 'a' to queue = ['c','b']. Pop queue. visited = ['a','b']
- Add neighbors of 'b' to queue = ['c','d']
+ Add neighbors of 'a' to queue = ['c','b']. Remove 1st element from queue. visited = ['a','c']
+ Add neighbors of 'c' to queue = ['b','e']. Remove 1st element from queue. visited = ['a','c','b']
+ Add neighbors of 'b' to queue = ['e','d']. Remove 1st element from queue. visited = ['a','c','b','e']
+ Add neighbors of 'e' to queue but 'e' has no children. Current queue = ['d']. Remove 1st element from queue.
+ visited = ['a','c','b','e','d']
+ Add neighbors of 'd' to queue = ['f']. Remove 1st element from queue.  visited = ['a','c','b','e','d','f']
 """
 
 def depthFirst(graph,source):
@@ -26,14 +30,12 @@ def depthFirst(graph,source):
 def depthFirstRecursive(graph,source):
 
     print(source)
-
     for neighbor in graph[source]:
         depthFirstRecursive(graph,neighbor)
 
 def breadthFirst(graph,source):
 
     queue = [source]
-
     while len(queue)>0:
         current = queue.pop(0)
         print(current)
@@ -50,5 +52,5 @@ graph = {
 }
 
 depthFirst(graph,'a') #a,b,d,f,c,e
-depthFirstRecursive(graph,'a') #a,b,d,f,c,e
+depthFirstRecursive(graph,'a') #a,c,e,b,d,f
 breadthFirst(graph,'a') # a,c,b,e,d,f
